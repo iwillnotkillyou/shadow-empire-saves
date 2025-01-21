19f6f128ff755421f4828455eed7852ac37524c5
pavli <pavli@NOTEBOOK-PAVEL>
2025-01-21 20:14:10 +0100

    m

---
+++ main.py
@@ -0,0 +1,19 @@
+import subprocess
+import time
+
+def pull():
+    subprocess.run(["git", "pull"])
+
+def pushAll():
+    subprocess.run(["git", "add", "-A"])
+    subprocess.run(["git", "commit", "-m", "m"])
+    subprocess.run(["git", "push"])
+
+if __name__ == '__main__':
+    start = time.time()
+    while True:
+        current_time = time.time()
+        if current_time-start > 1:
+            print("checked")
+            pull()
+            pushAll()
