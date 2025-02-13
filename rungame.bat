4b7a1e3ccae91b587dad24afb00d8fede8ef87ee
pavli <pavli@NOTEBOOK-PAVEL>
2025-02-13 13:31:55 +0100

    m

---
+++ rungame.bat
@@ -0,0 +1,8 @@
+cd "C:\Users\pavli\Documents\My Games\Shadow Empire"
+git pull
+
+cd "C:\Games\Shadow.Empire.Build.16537802"
+start ShadowEmpire
+
+cd "C:\Users\pavli\Documents\My Games\Shadow Empire"
+python main.py
